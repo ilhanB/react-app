c40e2aeb4ab217d378477c6974d998ca6ef8686d
ilhan <ilhans70@gmail.com>
2022-09-05 18:14:08 +0300

    fix(fdgdg): dfgdfgdg
    dgdfg

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import {render, screen} from "@testing-library/react";
 import App from "./App";
-import React from 'react';
+import * as React from 'react';
 
 test("renders learn react link", () => {
-  const element = render(<App />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
